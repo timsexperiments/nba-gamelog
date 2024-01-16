1085dbbfd5034d6670e320a8adf2c0c2da0b2ba2
Tim <tim@timsexperiments.foo>
2024-01-16 01:49:15 -0600

    Convert scraper to not panic

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -2,22 +2,33 @@ package scraper
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/proto"
 )
 
 const shareButtonSelector = "#tgl_basic_sh > div > ul > li.hasmore > div > ul > li:nth-child(3) > button"
 
-// LoadTeamSeasonLog loads the game log for a given team and season.
 func LoadTeamSeasonLog(team string, season int) (string, error) {
 	url := fmt.Sprintf("https://www.basketball-reference.com/teams/%s/%d/gamelog", team, season)
 
-	browser := rod.New().MustConnect()
-	defer browser.MustClose()
+	browser := rod.New().Timeout(time.Second * 3)
+	err := browser.Connect()
+	if err != nil {
+		return "", fmt.Errorf("Failed to connect browser: %w", err)
+	}
+	defer browser.Close()
 
-	page := browser.MustPage(url)
-	page.MustWaitIdle()
-	page.MustScreenshot("debug.png")
+	page, err := browser.Page(proto.TargetCreateTarget{URL: url})
+	if err != nil {
+		return "", fmt.Errorf("Failed to open page: %w", err)
+	}
+
+	err = page.WaitIdle(time.Microsecond)
+	if err != nil {
+		return "", fmt.Errorf("Error waiting for page idle: %w", err)
+	}
 
 	// Button is hidden on the page so custom JavaScript needs to be used to
 	// access the element.
@@ -26,9 +37,9 @@ func LoadTeamSeasonLog(team string, season int) (string, error) {
 	// observer is used on the document to ensure that the JavaScript in the
 	// page is loaded and the button exists on the page. Some sort of JavaScript
 	// framework is being used which takes a while to load.
-	page.MustEval(fmt.Sprintf(`async () => {
+	_, err = page.Evaluate(&rod.EvalOptions{JS: fmt.Sprintf(`async () => {
 		const selector = '%s';
-		await new Promise(resolve => {
+		new Promise(resolve => {
 			const observer = new MutationObserver(mutations => {
 				const el = document.querySelector(selector);
 				if (el) {
@@ -37,15 +48,26 @@ func LoadTeamSeasonLog(team string, season int) (string, error) {
 					resolve();
 				}
 			});
-	
+
 			observer.observe(document.body, {
 				childList: true,
 				subtree: true
 			});
 		});
-	}`, shareButtonSelector))
+	}`, shareButtonSelector)})
+	if err != nil {
+		return "", fmt.Errorf("error executing script: %w", err)
+	}
 
-	content := page.MustElement("#csv_tgl_basic").MustText()
+	el, err := page.Element("#csv_tgl_basic")
+	if err != nil {
+		return "", fmt.Errorf("failed to find element: %w", err)
+	}
+
+	content, err := el.Text()
+	if err != nil {
+		return "", fmt.Errorf("error getting text: %w", err)
+	}
 
 	return content, nil
 }
